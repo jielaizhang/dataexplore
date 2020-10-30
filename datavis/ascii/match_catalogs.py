eba4738d2abc0b030396ec27c400c83a6dfba948
Jielai Zhang <zhang.jielai@gmail.com>
2020-10-30 11:20:30 +1100

    second version that allows input of pandas DataFrames instead of ascii file, also allows radius threshold to be a lower or an upper one

--- datavis/ascii/match_catalogs.py
+++ datavis/ascii/match_catalogs.py
@@ -16,8 +16,8 @@ def match_catalogs(f_cat_ref, f_cat_sci,radius_threshold=2 * u.arcsec,
     cat_sci = ascii.read(f_cat_sci)
     df_ref  = pd.DataFrame(cat_ref.as_array())
     df_sci  = pd.DataFrame(cat_sci.as_array())
-    coords_ref = SkyCoord(ra=df_ref['X_WORLD'], dec=df_ref['Y_WORLD'],unit='deg')
-    coords_sci = SkyCoord(ra=df_sci['X_WORLD'], dec=df_sci['Y_WORLD'],unit='deg')
+    coords_ref = SkyCoord(ra=df_ref[ref_RA_KEY], dec=df_ref[ref_DEC_KEY],unit='deg')
+    coords_sci = SkyCoord(ra=df_sci[sci_RA_KEY], dec=df_sci[sci_DEC_KEY],unit='deg')
     
     # index = nearest in csci that matches cref
     # index = for each cref, get nearest csci
@@ -26,6 +26,35 @@ def match_catalogs(f_cat_ref, f_cat_sci,radius_threshold=2 * u.arcsec,
     
     # Sep constraint
     sep_constraint = d2d < radius_threshold
+
+    # Get entries in cat_ref with a match
+    df_ref_matched = df_ref[sep_constraint]
+    
+    # Get matched entries in cat_sci
+    df_sci_matched = df_sci.iloc[idx[sep_constraint]]
+    
+    return df_ref_matched, df_sci_matched
+
+def match_catalogs_df(df_ref, df_sci,radius_threshold=2 * u.arcsec,
+               ref_RA_KEY='X_WORLD',ref_DEC_KEY='Y_WORLD',
+               sci_RA_KEY='X_WORLD',sci_DEC_KEY='Y_WORLD',
+               find_close=True):
+    '''Input pandas data frame object for ref and sci catalogs, output pandas DataFrame of 
+    matched ref catalog entries and matched sci catalog entries. All columns in inputs are preserved.
+    '''
+    coords_ref = SkyCoord(ra=df_ref[ref_RA_KEY], dec=df_ref[ref_DEC_KEY],unit='deg')
+    coords_sci = SkyCoord(ra=df_sci[sci_RA_KEY], dec=df_sci[sci_DEC_KEY],unit='deg')
+    
+    # index = nearest in csci that matches cref
+    # index = for each cref, get nearest csci
+    # len(idx) = len(df_ref)
+    idx, d2d, d3d = coords_ref.match_to_catalog_3d(coords_sci)
+    
+    # Sep constraint
+    if find_close:
+        sep_constraint = d2d < radius_threshold
+    else:
+        sep_constraint = d2d > radius_threshold
 
     # Get entries in cat_ref with a match
     df_ref_matched = df_ref[sep_constraint]
