5125758be73a88b1fe847790374e0d9e1d5d0959
Jielai Zhang <zhang.jielai@gmail.com>
2020-11-12 15:10:47 +1100

    works

---
+++ datastats/run_sourceextractor.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+
+"""run_sourceextractor.py -- read in fits files, get catalog with spread model.
+
+Usage: run_sourceextractor [-h] [-q] [--debug] [-v] [-s LOC] [-p LOC] [--spreadmodel] [--savecats LOC] [--catending STRING] <fitsfiles>...
+
+Options:
+    -h, --help                  Show this screen
+    -q, --quiet                 Quiet mode, suppress printout of parameters measured.
+    --debug                     Show extra information and save extra files, including the .cat files [default: False]
+    -v, --verbose               Show extra information [default: False]     
+    -s LOC, --sextractor LOC    Location of source extractor [default: /opt/local/bin/source-extractor] 
+    -p LOC, --psfex LOC         Location of PSFEx [default: /opt/local/bin/psfex]
+    --spreadmodel               If on, spread_model, mag_model, magerr_model will be in output catalog [default: False]
+    --savecats LOC              Output cat files to this directory. If not input, outputs to same directory as input file.
+    --catending STRING          Output cats for each file.fits will be file_STRING.cat, if empty, output will be file.cat.
+
+Examples:
+    python run_sourceextractor.py *fits -o ./outfile.txt --savecats ../cats
+    from datastats.run_sourceextractor import run_sourceextractor
+        cats = run_sourceextractor(fitsfiles,
+                                    extractorloc='/opt/local/bin/source-extractor',
+                                    psfexloc='/opt/local/bin/psfex',
+                                    verbose=False,quietmode=False,
+                                    debugmode=False)
+"""
+
+import docopt, os
+import astropy.io.fits as fits
+import subprocess
+from astropy.io import ascii
+import pandas as pd
+import numpy as np
+import ntpath, shutil
+import time
+
+# Jielai Modules
+from datastats.measure_psf import measure_psf
+from misc.write_sourceextractor_files import write_sourceextractor_files
+
+
+####################### Auxiliary Functions #######################
+def remove_temp_files(fs):
+    for f in fs:
+        os.remove(f)
+    return None
+
+def mkdir_ifnotexist(directory):
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    return None
+
+####################### MAIN function #######################
+
+def run_sourceExtractor(fitsfiles,spreadmodel=False,
+                        savecats_dir = None, catending = None,
+                        sextractorloc='/opt/local/bin/source-extractor',
+                        psfexloc='/opt/local/bin/psfex',
+                        verbose=False,quietmode=False,debugmode=False):
+
+    if savecats_dir == None:
+        savecats_dir = './'
+
+    # Create temporary files required for Source Extractor
+    (nnw_path, 
+    conv_path, 
+    params_path, 
+    config_path) = write_sourceextractor_files('./','run_sourceextractor',spreadmodel=True,
+                                                sextractorloc=sextractorloc,quietmode=quietmode)
+    # Print useful information if verbose
+    if verbose:
+        if spreadmodel:
+            print('The Steps for 1 file at a time are:')
+            print('1. Measure PSF.')
+            print('1a. Run Source Extractor')
+            print('1b. Run PSFEx')
+            print('2. Run Source Extractor with spread_model as a parameter output')
+        print('#####################################')
+        print('###### RUNNING SOURCE EXTRACTOR #####')
+        print('#####################################\n')
+
+    # Set Source Extractor verbose type
+    if verbose:
+        VERBOSE_TYPE = 'NORMAL'
+    else:
+        VERBOSE_TYPE = 'QUIET'
+
+    # Run Source Extractor one image at a time
+    catfiles = []
+    psffiles = []
+    catted_fitsfiles = []
+    PSF_success = [False]*len(fitsfiles)
+    for ii, f in enumerate(fitsfiles):
+
+        # Determine where cat file is to be saved (full path including file name)
+        fname       = ntpath.basename(f)
+        if catending:
+            catalog_name = savecats_dir+os.path.sep+fname.replace('.fits','_'+catending+'.cat')
+        else:
+            catalog_name = savecats_dir+os.path.sep+fname.replace('.fits','.cat')
+
+        # Print separator
+        if verbose:
+            print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+            print(f'Input {f}')
+            print(f'Intended output {catalog_name}')
+            print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+
+        # Run Source Extractor then PSFEx
+        if spreadmodel:
+            try:
+                if verbose:
+                    print('Currently measuring PSF using source extractor and psfex...')
+                [f_psf] = measure_psf([f], outdir=savecats_dir, savepsffits=False,
+                                        sextractorloc=sextractorloc,
+                                        psfexloc=psfexloc,
+                                        verbose=verbose,quietmode=True)
+                PSF_success[ii] = True
+            except:
+                print(f'\nSKIPPED: PSF measurement unsuccessful for {f}')
+                continue
+        else:
+            # If spread_model is false, then don't need to run it, so set as true to run below source extraction. 
+            PSF_success[ii] = True
+
+        if PSF_success[ii] == True:
+            # Run Source Extractor on image to get final catalog output
+            command =   f'{sextractorloc} -c {config_path} '\
+                        f'-CATALOG_NAME {catalog_name} '\
+                        f'-CATALOG_TYPE ASCII_HEAD '\
+                        f'-PARAMETERS_NAME {params_path} -FILTER_NAME {conv_path} '\
+                        f'-STARNNW_NAME {nnw_path} -MAG_ZEROPOINT 25.0 '\
+                        f'-PSF_NAME {f_psf} -PSF_NMAX 1 -PATTERN_TYPE GAUSS-LAGUERRE '\
+                        f'-VERBOSE_TYPE {VERBOSE_TYPE} '\
+                        f'{f}'
+            if verbose:
+                print('Currently running source extractor to output required catalog...')
+                print('=============================================')
+                print('Executing command: %s\n' % command)
+            try:
+                rval = subprocess.run(command.split(), check=True)
+                catfiles.append(catalog_name)
+                catted_fitsfiles.append(f)
+                os.remove(f_psf)
+                if verbose:
+                    print(f'Success! Cat saved: {catalog_name}')
+            except subprocess.CalledProcessError as err:
+                print('\nCould not run SExtractor with exit error %s\n'%err)
+                print('Command used:\n%s\n'%command)
+
+    # Remove temporary files required for Source Extractor
+    remove_temp_files([nnw_path,conv_path,params_path,config_path])
+    
+    return catfiles, catted_fitsfiles 
+
+############################################################################
+####################### BODY OF PROGRAM STARTS HERE ########################
+#######################              MAIN           ########################
+############################################################################
+
+if __name__ == "__main__":
+
+    arguments = docopt.docopt(__doc__)
+
+    # Mandatory argument
+    fitsfiles = arguments['<fitsfiles>']
+
+    # Non-mandatory options without arguments
+    quietmode       = arguments['--quiet']
+    verbose         = arguments['--verbose']
+    debugmode       = arguments['--debug']
+    savecats_dir    = arguments['--savecats']
+    catending       = arguments['--catending']
+    sextractorloc   = arguments['--sextractor']
+    psfexloc        = arguments['--psfex']
+    spreadmodel     = arguments['--spreadmodel']
+
+    # Calculate
+    _,_ = run_sourceExtractor(fitsfiles,spreadmodel=spreadmodel,
+                                sextractorloc=sextractorloc,
+                                psfexloc=psfexloc,
+                                savecats_dir = savecats_dir, catending = catending,
+                                verbose=verbose,quietmode=quietmode,debugmode=debugmode)
