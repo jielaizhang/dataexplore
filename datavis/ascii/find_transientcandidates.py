6bdf3aaead3224f9b70ebc454eb286bdb0919125
Jielai Zhang <zhang.jielai@gmail.com>
2021-01-04 18:39:12 +1100

    fixed a bug when there are no candidates in a certain round; now actually numbered 3 rounds of candidate detection (sub; sci/sub; sci/sub and no neg are the 3 rounds), and there are true/false checks for stuff relevant to each round now. restructured code a bit to make this clearer.

--- datavis/ascii/find_transientcandidates.py
+++ datavis/ascii/find_transientcandidates.py
@@ -113,6 +113,44 @@ def write_empty_ds9commands_file(ds9commands_file,sub_fits,sci_fits,neg_fits):
 
     return ds9commands_file
 
+def select_rightRADEC(df_sci,df_pos,df_neg,RAmin,RAmax,DECmin,DECmax,verbose=False):
+
+    if RAmax:
+        RAmax = float(RAmax)
+        RAmin = float(RAmin)
+        DECmax = float(DECmax)
+        DECmin = float(DECmin)
+
+        df_sci_RADECselect = df_sci[(   (df_sci['X_WORLD']>RAmin) &
+                                        (df_sci['X_WORLD']<RAmax) &
+                                        (df_sci['Y_WORLD']>DECmin) &
+                                        (df_sci['Y_WORLD']<DECmax) 
+                                    )
+                                    ]
+        df_pos_RADECselect = df_pos[(   (df_pos['X_WORLD']>RAmin) &
+                                        (df_pos['X_WORLD']<RAmax) &
+                                        (df_pos['Y_WORLD']>DECmin) &
+                                        (df_pos['Y_WORLD']<DECmax) 
+                                    )
+                                    ]
+        df_neg_RADECselect = df_neg[(   (df_neg['X_WORLD']>RAmin) &
+                                        (df_neg['X_WORLD']<RAmax) &
+                                        (df_neg['Y_WORLD']>DECmin) &
+                                        (df_neg['Y_WORLD']<DECmax) 
+                                    )
+                                    ]
+        if verbose:
+            print(f'Sources within specified RA, DEC range in SCI: {len(df_sci_RADECselect)}')
+            print(f'Sources within specified RA, DEC range in POS: {len(df_pos_RADECselect)}')
+            print(f'Sources within specified RA, DEC range in NEG: {len(df_neg_RADECselect)}')
+
+    else:
+        df_sci_RADECselect = df_sci
+        df_pos_RADECselect = df_pos
+        df_neg_RADECselect = df_neg
+
+    return df_sci_RADECselect,df_pos_RADECselect,df_neg_RADECselect
+
 def find_transientCandidates_(sub_cat, sci_cat, neg_cat, 
                               sub_fits,sci_fits,neg_fits,
                               savedir='./',
@@ -176,39 +214,7 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
 
     # ..............................................................
     # Select sources that's within RA DEC Range
-    if RAmax:
-        RAmax = float(RAmax)
-        RAmin = float(RAmin)
-        DECmax = float(DECmax)
-        DECmin = float(DECmin)
-
-        df_sci_RADECselect = df_sci[(   (df_sci['X_WORLD']>RAmin) &
-                                        (df_sci['X_WORLD']<RAmax) &
-                                        (df_sci['Y_WORLD']>DECmin) &
-                                        (df_sci['Y_WORLD']<DECmax) 
-                                    )
-                                    ]
-        df_pos_RADECselect = df_pos[(   (df_pos['X_WORLD']>RAmin) &
-                                        (df_pos['X_WORLD']<RAmax) &
-                                        (df_pos['Y_WORLD']>DECmin) &
-                                        (df_pos['Y_WORLD']<DECmax) 
-                                    )
-                                    ]
-        df_neg_RADECselect = df_neg[(   (df_neg['X_WORLD']>RAmin) &
-                                        (df_neg['X_WORLD']<RAmax) &
-                                        (df_neg['Y_WORLD']>DECmin) &
-                                        (df_neg['Y_WORLD']<DECmax) 
-                                    )
-                                    ]
-        if verbose:
-            print(f'Sources within specified RA, DEC range in SCI: {len(df_sci_RADECselect)}')
-            print(f'Sources within specified RA, DEC range in POS: {len(df_pos_RADECselect)}')
-            print(f'Sources within specified RA, DEC range in NEG: {len(df_neg_RADECselect)}')
-
-    else:
-        df_sci_RADECselect = df_sci
-        df_pos_RADECselect = df_pos
-        df_neg_RADECselect = df_neg
+    df_sci_RADECselect,df_pos_RADECselect,df_neg_RADECselect = select_rightRADEC(df_sci,df_pos,df_neg,RAmin,RAmax,DECmin,DECmax,verbose=verbose)
 
     # ..............................................................
     # Make cuts on sources detected
@@ -245,40 +251,57 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
     # source in Subtraction, but no nearby source in Negative.
     # XXX Hard coded section !!! (radius_threshold)
 
+    # Track if there are candidates for each round before proceeding. 
     no_candidates = False
+    no_sciYes_posYes_candidates = False
+    no_sciYes_posYes_negNo_candidates = False
+
+    # Round 1: some positive detections made
     if len(df_pos_select) == 0:
         print('No subtraction sources has passed tests up to this point.')
-        no_candidates = True
+        no_candidates                     = True
+        no_sciYes_posYes_candidates       = True
+        no_sciYes_posYes_negNo_candidates = True
     else:
         # Get sci and sub matched
         radius_threshold = 2.5 * u.arcsec # 10 pixels for KMTNet
         df_sciYes_posYes, df_sciYes_posYes_sub = match_catalogs_df( df_sci_select, df_pos_select,
                                                                     radius_threshold=radius_threshold)
-        # Get sci/sub matched with no nearby neg
-        radius_threshold = 4 * u.arcsec
-        df_sciYes_posYes_negNo, df_sciYes_posYes_negNo_neg = match_catalogs_df( df_sciYes_posYes, df_neg_select,
-                                                                                radius_threshold=radius_threshold,find_close=False)
-        # Grab the corresponding sub too (this line doesn't change df_sciYes_posYes_negNo)
-        df_sciYes_posYes_negNo, df_sciYes_posYes_negNo_sub = match_catalogs_df( df_sciYes_posYes_negNo, df_pos_select,
-                                                                                radius_threshold=radius_threshold,find_close=True)
 
-        if verbose:
+    # Round 2: for each science detection there is a positive detections associated
+    if no_candidates == False:
+        if len(df_sciYes_posYes) == 0:
+            print('No positive sources with matching subtraction sources has passed tests up to this point.')
+            no_sciYes_posYes_candidates       = True
+            no_sciYes_posYes_negNo_candidates = True
+        else:        
+            # Get sci/sub matched with no nearby neg
+            radius_threshold = 4 * u.arcsec
+            df_sciYes_posYes_negNo, df_sciYes_posYes_negNo_neg = match_catalogs_df( df_sciYes_posYes, df_neg_select,
+                                                                                    radius_threshold=radius_threshold,find_close=False)
+            # Grab the corresponding sub too (this line doesn't change df_sciYes_posYes_negNo)
+            df_sciYes_posYes_negNo, df_sciYes_posYes_negNo_sub = match_catalogs_df( df_sciYes_posYes_negNo, df_pos_select,
+                                                                                    radius_threshold=radius_threshold,find_close=True)
+
+    # Round 3: for each sci/sub pair, there is no nearby negative detection
+    if no_sciYes_posYes_candidates == False:
+        if len(df_sciYes_posYes_negNo) == 0:
+            print('No positive sources with matching subtraction sources with no nearby negative sources has passed tests up to this point.')
+            no_sciYes_posYes_negNo_candidates = True
+            
+    if verbose:
+        if no_sciYes_posYes_candidates == False:
             print(f'Sources in SCI matched in POS SUB: {len(df_sciYes_posYes)}')
+        if no_sciYes_posYes_negNo_candidates == False:
             print(f'Sources in SCI matched in POS SUB, then not matched in NEG: {len(df_sciYes_posYes_negNo)}')
-
-
-    # ..............................................................
-    # Open ds9commands file for overwriting
-    f_ds9 = open(ds9commands_file, "w")
-    f_ds9.write("# ====================================================\n")
-    f_ds9.write(f'# {sub_cat}')
 
     # ..............................................................
     # Make Region files of SELECTED sources in Sci, Sub, Neg
+    # If sub catalog successfully read in, and this function wasn't terminated, there will be sources to write into these region files.
 
     # Sub Detections Selected (Green squares)
-    Rs = df_pos_select['X_WORLD']
-    Ds = df_pos_select['Y_WORLD']
+    Rs = np.array(df_pos_select['X_WORLD'])
+    Ds = np.array(df_pos_select['Y_WORLD'])
     Widths = ["0.09'"]*len(Rs)
     Heights = ["0.00'"]*len(Rs)
     Angles = ["0.0"]*len(Rs)
@@ -290,8 +313,8 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
     np.savetxt(reg_pos_temp, (savetext),fmt='%s',header=h)
 
     # Neg Detections Selected (Red squares, bit bigger than Sub Squares)
-    Rs = df_neg_select['X_WORLD']
-    Ds = df_neg_select['Y_WORLD']
+    Rs = np.array(df_neg_select['X_WORLD'])
+    Ds = np.array(df_neg_select['Y_WORLD'])
     Widths = ["0.11'"]*len(Rs)
     Heights = ["0.11'"]*len(Rs)
     Angles = ["0.0"]*len(Rs)
@@ -303,8 +326,8 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
     np.savetxt(reg_neg_temp, (savetext),fmt='%s',header=h)
 
     # Sci Detection Selected (Blue squares, big bigger than Sub Squares, at 30 deg angle)
-    Rs = df_sci_select['X_WORLD']
-    Ds = df_sci_select['Y_WORLD']
+    Rs = np.array(df_sci_select['X_WORLD'])
+    Ds = np.array(df_sci_select['Y_WORLD'])
     Widths = ["0.11'"]*len(Rs)
     Heights = ["0.11'"]*len(Rs)
     Angles = ["30.0"]*len(Rs)
@@ -330,26 +353,14 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
     os.remove(reg_sci_temp)
     os.remove(reg_neg_temp)
 
-    # Add ds9 command to ds9 commands file.
-    f_ds9.write('# ---Sub, neg sub, sci, detected sources region file --- \n')
-    f_ds9.write('# green = sub\n')
-    f_ds9.write('# red = neg sub\n')
-    f_ds9.write('# blue 30 deg = sci\n')
-    if no_candidates == True:
-        f_ds9.write('# No candidates found.\n')
-    command =   f'ds9 -zscale -lock frame wcs {sub_fits} {neg_fits} {sci_fits} '\
-                f'-regions load all {reg_all_selected_detections} &\n\n' 
-    f_ds9.write(command)
-    f_ds9.write('')
-
 
     # ..............................................................
     # Make Region files of sources matched in Sci and Sub
 
-    if no_candidates == False:
+    if no_sciYes_posYes_candidates == False:
         # Save region file.
-        Rs_sciYes_posYes = df_sciYes_posYes['X_WORLD']
-        Ds_sciYes_posYes = df_sciYes_posYes['Y_WORLD']
+        Rs_sciYes_posYes = np.array(df_sciYes_posYes['X_WORLD'])
+        Ds_sciYes_posYes = np.array(df_sciYes_posYes['Y_WORLD'])
         Sizes   = ["0.1'"]*len(Rs_sciYes_posYes)
         Shapes = ['circle']*len(Rs_sciYes_posYes)
         j2000s  = ['j2000;']*len(Rs_sciYes_posYes)
@@ -360,19 +371,11 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
         if verbose:
             print(f'Saved: {reg_sciYes_subYes}')
 
-        # Add ds9 command to ds9 commands file.
-        f_ds9.write('# ---Sub, sci, matched sources (sciYes_posYes) region file --- \n')
-        f_ds9.write('# green circle = sci/sub matched\n')
-        command =   f'ds9 -zscale -lock frame wcs {sub_fits} {sci_fits} '\
-                    f'-regions load all {reg_sciYes_subYes} & \n\n'    
-        f_ds9.write(command)
-        f_ds9.write('')
+    # ..............................................................
+    # Make Region files of sources matched in Sci and Sub and not in Neg
+    # XXX Hard coded section !!! (FRB200914 error region)
 
-
-        # ..............................................................
-        # Make Region files of sources matched in Sci and Sub and not in Neg
-        # XXX Hard coded section !!! (FRB200914 error region)
-
+    if no_sciYes_posYes_negNo_candidates == False:
         # Save region file, include FRB200914 error region 
         Rs_sciYes_posYes_negNo = np.array(df_sciYes_posYes_negNo['X_WORLD'])
         Ds_sciYes_posYes_negNo = np.array(df_sciYes_posYes_negNo['Y_WORLD'])
@@ -395,23 +398,8 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
         if verbose:
             print(f'Saved: {region_file}')
 
-        # Add ds9 command to ds9 commands file- sub sci and neg
-        f_ds9.write('# ---Sub, sci, neg; sciYes_posYes_negNo region file --- \n')
-        f_ds9.write('# yellow panda = sci/sub matched but not matched to neg\n')
-        command =   f'ds9 -zscale -lock frame wcs {sub_fits} {sci_fits} {neg_fits} '\
-                    f'-regions load all {region_file} &\n\n'    
-        f_ds9.write(command)
-        f_ds9.write('')
 
-        # Add ds9 command to ds9 commands file- sub sci only
-        f_ds9.write('# ---Sub, sci; sciYes_posYes_negNo region file --- \n')
-        f_ds9.write('# yellow panda = sci/sub matched but not matched to neg\n')
-        command =   f'ds9 -zscale -lock frame wcs {sub_fits} {sci_fits} '\
-                    f'-regions load all {region_file} &\n\n'    
-        f_ds9.write(command)
-        f_ds9.write('')
-
-    elif no_candidates == True:
+    elif no_sciYes_posYes_candidates == True:
 
         # Write region file only with error region. 
         fp = open(region_file, "w")
@@ -419,6 +407,60 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
         fp.write('# No candidates detected.')
         fp.write("j2000; circle 326.65958333 -81.0772222 7.5' #color=yellow")
         fp.close()
+
+
+    # ..............................................................
+    # Open ds9commands file 
+    f_ds9 = open(ds9commands_file, "w")
+    f_ds9.write("# ====================================================\n")
+    f_ds9.write(f'# {sub_cat}\n')
+    if no_candidates == True:
+        f_ds9.write('# No candidates found.\n')
+
+    # Add ds9 command to ds9 commands file: all detections
+    f_ds9.write('# ---Sub, neg sub, sci, detected sources region file --- \n')
+    f_ds9.write('# green = sub\n')
+    f_ds9.write('# red = neg sub\n')
+    f_ds9.write('# blue 30 deg = sci\n')
+    command =   f'ds9 -zscale -lock frame wcs {sub_fits} {neg_fits} {sci_fits} '\
+                f'-regions load all {reg_all_selected_detections} &\n\n' 
+    f_ds9.write(command)
+    f_ds9.write('')
+
+    # Add ds9 command to ds9 commands file: sciYes subYes
+    if no_sciYes_posYes_candidates == False:
+        f_ds9.write('# ---Sub, sci, matched sources (sciYes_posYes) region file --- \n')
+        f_ds9.write('# green circle = sci/sub matched\n')
+        command =   f'ds9 -zscale -lock frame wcs {sub_fits} {sci_fits} '\
+                    f'-regions load all {reg_sciYes_subYes} & \n\n'    
+        f_ds9.write(command)
+        f_ds9.write('')
+
+    # Add ds9 command to ds9 commands file: sciYes subYes negNo
+    if no_sciYes_posYes_negNo_candidates == False:
+        f_ds9.write('# ---Sub, sci, neg; sciYes_posYes_negNo region file --- \n')
+        f_ds9.write('# yellow panda = sci/sub matched but not matched to neg\n')
+        command =   f'ds9 -zscale -lock frame wcs {sub_fits} {sci_fits} {neg_fits} '\
+                    f'-regions load all {region_file} &\n\n'    
+        f_ds9.write(command)
+        f_ds9.write('')
+
+    # Add ds9 command to ds9 commands file: sciYes subYes negNo, but sub sci only
+    if no_sciYes_posYes_negNo_candidates == False:
+        f_ds9.write('# ---Sub, sci; sciYes_posYes_negNo region file --- \n')
+        f_ds9.write('# yellow panda = sci/sub matched but not matched to neg\n')
+        command =   f'ds9 -zscale -lock frame wcs {sub_fits} {sci_fits} '\
+                    f'-regions load all {region_file} &\n\n'    
+        f_ds9.write(command)
+        f_ds9.write('')
+
+    # Close ds9commands file 
+    f_ds9.close()  
+    if verbose:
+        print(f'Saved: {ds9commands_file}')
+
+    # ..............................................................
+    # Create sciYes subYes noNeg candidates list
 
     # Candidate pandas table
     h               =   'RA_sci DEC_sci RA_sub DEC_sub '\
@@ -434,7 +476,7 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
                         'CLASS_STAR_sci CLASS_STAR_sub '\
                         'SPREAD_MODEL_sci SPREAD_MODEL_sub'
 
-    if no_candidates == False:
+    if no_sciYes_posYes_negNo_candidates == False:
         RA_sci              = np.array(df_sciYes_posYes_negNo['X_WORLD'])
         DEC_sci             = np.array(df_sciYes_posYes_negNo['Y_WORLD'])
         FLUX_RADIUS_sci     = np.array(df_sciYes_posYes_negNo['FLUX_RADIUS'])
@@ -487,7 +529,7 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
         candidate_list  = np.transpose(aaa)
         np.savetxt(candidate_list_file, (candidate_list),fmt='%s',header=h)
 
-    elif no_candidates == True:
+    elif no_sciYes_posYes_negNo_candidates == True:
         # Write empty candidate list file
         fp = open(candidate_list_file, "w")
         fp.write('# No candidates detected.\n')
@@ -499,13 +541,6 @@ def find_transientCandidates_(sub_cat, sci_cat, neg_cat,
 
     if verbose:
         print(f'Saved: {candidate_list_file}') 
-    
-    # ..............................................................
-    # Close ds9commands file 
-    f_ds9.close()  
-    if verbose:
-        print(f'Saved: {ds9commands_file}')
-
 
     # ..............................................................
     return region_file, candidate_list_file, candidate_list, ds9commands_file
