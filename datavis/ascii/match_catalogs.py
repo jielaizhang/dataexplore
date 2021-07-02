233ae35f33d08eda234fd3b34e79d76b27bbd895
Jielai Zhang <zhang.jielai@gmail.com>
2021-07-02 21:11:40 +1000

    works

--- datavis/ascii/match_catalogs.py
+++ datavis/ascii/match_catalogs.py
@@ -6,6 +6,8 @@ import pandas as pd
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 
+# from datavis.ascii.match_catalogs import find_closest_in_cat
+
 def match_catalogs(f_cat_ref, f_cat_sci,radius_threshold=2 * u.arcsec,
                ref_RA_KEY='X_WORLD',ref_DEC_KEY='Y_WORLD',
                sci_RA_KEY='X_WORLD',sci_DEC_KEY='Y_WORLD'):
@@ -63,3 +65,54 @@ def match_catalogs_df(df_ref, df_sci,radius_threshold=2 * u.arcsec,
     df_sci_matched = df_sci.iloc[idx[sep_constraint]]
     
     return df_ref_matched, df_sci_matched
+
+def find_closest_in_cat(RA,DEC, df_sci,radius_threshold=2 * u.arcsec,
+               sci_RA_KEY='X_WORLD',sci_DEC_KEY='Y_WORLD'):
+    '''Input pandas DataFrame catalog, output line in DataFrame closest in input RA,DEC. 
+    All columns in inputs are preserved.
+    '''
+    try:
+        x=len(RA)
+        coords_ref = SkyCoord(ra=RA, dec=DEC,unit='deg')
+    except:
+        coords_ref = SkyCoord(ra=[RA], dec=[DEC],unit='deg')
+    coords_sci = SkyCoord(ra=df_sci[sci_RA_KEY], dec=df_sci[sci_DEC_KEY],unit='deg')
+    
+    # index = nearest in csci that matches input RA,DEC
+    # index = for each input RA, DEC, get nearest csci
+    # len(idx) = len(RA)
+    idx, d2d, d3d = coords_ref.match_to_catalog_3d(coords_sci)
+    
+    # Sep constraint
+    sep_constraint = d2d < radius_threshold
+    
+    # Get matched entries in cat_sci
+    df_sci_matched = df_sci.iloc[idx[sep_constraint]]
+    
+    return df_sci_matched
+
+
+def find_all_within(df_ref, RA,DEC,radius_threshold=2 * u.arcsec,
+               ref_RA_KEY='X_WORLD',ref_DEC_KEY='Y_WORLD',
+               find_close=True):
+    '''Input pandas DataFrame object for ref, output all lines in pandas DataFrame within radius threshold of RA and DEC. 
+    All columns in inputs are preserved.
+    '''
+    coords_ref = SkyCoord(ra=df_ref[ref_RA_KEY], dec=df_ref[ref_DEC_KEY],unit='deg')
+    coords_sci = SkyCoord(ra=[RA], dec=[DEC],unit='deg')
+    
+    # index = nearest in csci that matches cref
+    # index = for each cref, get nearest csci
+    # len(idx) = len(df_ref)
+    idx, d2d, d3d = coords_ref.match_to_catalog_3d(coords_sci)
+    
+    # Sep constraint
+    if find_close:
+        sep_constraint = d2d < radius_threshold
+    else:
+        sep_constraint = d2d > radius_threshold
+
+    # Get entries in cat_ref with a match
+    df_ref_matched = df_ref[sep_constraint]
+    
+    return df_ref_matched
