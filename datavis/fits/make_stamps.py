985e57c71fdbe83f4308a3d7d4838ec32bc3e2f7
Jielai Zhang <zhang.jielai@gmail.com>
2021-10-06 20:46:59 +1100

    update

--- datavis/fits/make_stamps.py
+++ datavis/fits/make_stamps.py
@@ -7,32 +7,39 @@ from astropy.visualization import ZScaleInterval
 from astropy.visualization import ImageNormalize
 
 
-
-def make_stamps(RA,DEC,fitsfiles_2Darray,outpng='stamp.png',labels=False,size=50,debug=False,axisticks=False):
+def make_stamps(RA,DEC,fitsfiles_2Darray,output='stamp.png',labels=False,size=50,debug=False,
+                axisticks=False,mark_target=False):
     '''fitsfiles_2Darray should be a 2D array that specifies the number of rows and columns in output stamps.
     E.g. [[temp_1.fits,temp_2.fits],[sci_1.fits,sci2.fits],[sub_1.fits,sub_2.fits]]
     '''
+            
+    ##################
+    # Set up subplot #
+    ##################
     n_cols = np.shape(fitsfiles_2Darray)[0]
     n_rows = np.shape(fitsfiles_2Darray)[1]
     if debug:
         print('n_cols, n_rows:',n_cols,n_rows)
-        
-    # Get WCS 
-    f   = fitsfiles_2Darray[0][0]
-    _,h = fits.getdata(f,header=True)
-    wcs = WCS(h)
-    (X, Y)       = wcs.wcs_world2pix(RA, DEC, 0)
-    X,Y          = [int(x) for x in [X,Y]]
-    wcs_cutout   = wcs[X-size:X+size,Y-size:Y+size] 
-    
-    # Set up subplot
+
     if axisticks == True:
+        # Get WCS onto axis
+        f   = fitsfiles_2Darray[0][0]
+        _,h = fits.getdata(f,header=True)
+        wcs = WCS(h)
+        (X, Y)       = wcs.wcs_world2pix(RA, DEC, 0)
+        X,Y          = [int(x) for x in [X,Y]]
+        wcs_cutout   = wcs[X-size:X+size,Y-size:Y+size] 
+        # start figure
         fig, axs = plt.subplots(n_rows, n_cols,figsize=(n_cols*5,n_rows*5),
                                 subplot_kw={'projection': wcs_cutout, 'label':"overlays"})
     else:
+        # start figure
         fig, axs = plt.subplots(n_rows, n_cols,figsize=(n_cols*5,n_rows*5))
     fig.subplots_adjust(left=None, bottom=None, right=1., top=None, wspace=None, hspace=None)
     
+    ##############################
+    # plot one subplot at a time #
+    ##############################
     for row in range(n_rows):
         for col in range(n_cols):
             if debug:
@@ -44,11 +51,11 @@ def make_stamps(RA,DEC,fitsfiles_2Darray,outpng='stamp.png',labels=False,size=50
             d,h = fits.getdata(f,header=True)
             
             # Do cutout
-            wcs = WCS(h)
-            (X, Y)       = wcs.wcs_world2pix(RA, DEC, 0)
-            X,Y          = [int(x) for x in [X,Y]]
-            image_cutout = d[X-size:X+size,Y-size:Y+size]
-            wcs_cutout   = wcs[X-size:X+size,Y-size:Y+size] 
+            wcs                          = WCS(h)
+            (X, Y)                       = wcs.wcs_world2pix(RA, DEC, 0)
+            X,Y                          = [int(x) for x in [X,Y]]
+            image_cutout                 = d[X-size:X+size,Y-size:Y+size]
+            wcs_cutout                   = wcs[X-size:X+size,Y-size:Y+size]
             
             # pixel value normalisation
             norm = ImageNormalize(image_cutout, interval=ZScaleInterval(),stretch=LinearStretch())
@@ -58,7 +65,6 @@ def make_stamps(RA,DEC,fitsfiles_2Darray,outpng='stamp.png',labels=False,size=50
                 axs[max(row,col)].imshow(image_cutout, norm = norm, cmap='gray') 
             else:
                 axs[row, col].imshow(image_cutout, norm = norm, cmap='gray') 
-            
             # labels
             if labels:
                 label=labels[col][row]
@@ -68,14 +74,16 @@ def make_stamps(RA,DEC,fitsfiles_2Darray,outpng='stamp.png',labels=False,size=50
             # axis ticks   
             if axisticks == True:
                 if n_rows == 1 or n_cols ==1:
+                    axs[max(row,col)].set_title(label, fontsize=10)
                     axs[max(row,col)].coords[1].set_major_formatter('d.ddd')
                     axs[max(row,col)].coords[0].set_major_formatter('d.ddd')
-                    axs[max(row,col)].coords[0].set_axislabel(f'\nRight Ascension \n {label} \n', fontsize=10)
+                    axs[max(row,col)].coords[0].set_axislabel(f'Right Ascension', fontsize=10)
                     axs[max(row,col)].coords[1].set_axislabel('Declination',fontsize=10)  
                 else:
+                    axs[row, col].set_title(label, fontsize=10)
                     axs[row, col].coords[1].set_major_formatter('d.ddd')
                     axs[row, col].coords[0].set_major_formatter('d.ddd')
-                    axs[row, col].coords[0].set_axislabel(f'\nRight Ascension \n {label} \n', fontsize=10)
+                    axs[row, col].coords[0].set_axislabel(f'Right Ascension', fontsize=10)
                     axs[row, col].coords[1].set_axislabel('Declination',fontsize=10)  
             else:
                 if n_rows == 1 or n_cols ==1:
@@ -84,5 +92,7 @@ def make_stamps(RA,DEC,fitsfiles_2Darray,outpng='stamp.png',labels=False,size=50
                 else:
                     axs[row, col].set_axis_off()
                     axs[row, col].set_title(label, fontsize=10)
+                    
+    plt.savefig(output)
 
-    return outpng
+    return output
